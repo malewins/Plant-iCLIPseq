228955dcf5b13c901413b26d289be5d54e4cbd77
malewins <malewins@cebitec.uni-bielefeld.de>
2023-10-16 11:19:32 +0200

    Update on UMI transfer script
    Transfer was coded more generally to fit differeent types of Read id structures.

--- workflow/02_QC_passed/01_transferUMI.sh
+++ workflow/02_QC_passed/01_transferUMI.sh
@@ -4,6 +4,6 @@ OUTDIR=01_correct_IDs
 mkdir -p $OUTDIR
 for FQ in *.gz;
 do
-  bioawk -c fastx '{print "@"$name"#"substr($comment,8) "\n"$seq"\n+\n"$qual}' $FQ | gzip >$OUTDIR/$FQ
+  bioawk -c fastx '{split($comment, UMI,"_"); print "@"$name"#"UMI[2] "\n"$seq"\n+\n"$qual}' $FQ | gzip >$OUTDIR/$FQ
 done
 
